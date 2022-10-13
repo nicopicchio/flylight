221650ceafc6f321b9f43df2fe5af0872e81f2ee
Kimberly Dijkmans <kim.14420@outlook.com>
2022-10-13 22:51:29 +0100

    Added start screen content and style

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './StartScreen.css';
+import logo from '../assets/Logo.png';
 
 export default function StartScreen() {
   return (
-    <div>StartScreen</div>
+    <div class="start-screen">
+      <img class="start-screen__logo" src={logo} alt="FlyLight logo"/>
+      <h1 class="start-screen__title">FlyLight</h1>
+      <p class="start-screen__description">An app that rewards flyers who pack lighter as their way to help reduce carbon emissions and fly more sustainable</p>
+      <button class="start-screen__button button-turquoise button-turquoise--main">Get started</button>
+    </div>
   )
 }
