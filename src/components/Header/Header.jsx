6c00fcbf64e92396f58e0cf7dff664d264d5cae9
Nico Picchio <nicolapicchio@gmail.com>
2022-10-14 15:04:55 +0000

    changed image on header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import '../Header/Header.css'
 import backIcon from '../../assets/Back-icon.svg'
 import Switch from '@mui/material/Switch';
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
     <div className='header-container'>
-      <img src={backIcon} alt="back button" />
+      <Link className='back-btn' to='/'>
+        <img src={backIcon} alt="back button" />
+      </Link>
       <div className='switch-container'>
         <Switch/>
         <p className='eco-mode'>ECO</p>
