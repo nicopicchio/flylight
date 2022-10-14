678c430528c684cfed4daa8c9b329818e8c68c51
Will <willcowendrums@gmail.com>
2022-10-14 11:38:48 +0100

    added react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
-import StartScreen from './pages/StartScreen/StartScreen';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import StartScreen from "./pages/StartScreen/StartScreen";
+import AddTripForm from "./pages/AddTripForm/AddTripForm";
 
 function App() {
   return (
-    <div className="App"> 
-
-      <StartScreen />
-
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<StartScreen />} />
+        <Route path="/addtrip" element={<AddTripForm />} />
+      </Routes>
     </div>
   );
 }
