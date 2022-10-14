bd1654fb6b3d0126d588ba5317ae6888290b4291
Kimberly Dijkmans <kim.14420@outlook.com>
2022-10-14 17:57:33 +0100

    Changed h3 to h2

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="home">
       <div className="home__weather">
-        <h3 className="home__weather__title">Weather for my next trip</h3>
+        <h2 className="home__weather__title">Weather for my next trip</h2>
         <div className="home__weather__items">
           {/* 5 day weather items, change to dynamic */}
           <Weather />
@@ -22,7 +22,7 @@ export default function Home() {
 
       <div className="home__my-trips">
         <div className="home__my-trips__header">
-          <h3 className="home__my-trips__title">My trips</h3>
+          <h2 className="home__my-trips__title">My trips</h2>
           <div className="home__my-trips__buttons">
             <Link to="/my-trips-summary" className="home__my-trips__button--see-all button-turquoise button-turquoise--small">See all</Link>
             <Link to="/add-trip" className="home__my-trips__button--add-trip button-grey"><PlusIcon /></Link>
@@ -37,7 +37,7 @@ export default function Home() {
 
       <div className="home__my-rewards">
         <div className="home__my-rewards__header">
-          <h3 className="home__my-rewards__title">My rewards</h3>
+          <h2 className="home__my-rewards__title">My rewards</h2>
           <Link to="/my-rewards" className="home__my-rewards__button--see-all button-turquoise button-turquoise--small">See all</Link>
         </div>
         {/* reward level item */}
