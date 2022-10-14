ef39c3668b2ee0d82fe6a0caf69a4b07e2cf2042
Will <willcowendrums@gmail.com>
2022-10-14 12:11:06 +0100

    progress on form

--- src/pages/AddTripForm/AddTripForm.jsx
+++ src/pages/AddTripForm/AddTripForm.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
 export default function AddTripForm() {
   return (
-    <div>AddTripForm</div>
-  )
+    <div className="addtripform">
+      <h1 className="addtripform--title">Add Trip</h1>
+      <h3 className="addtripform--subtitle">Enter Flight Details</h3>
+      <div className="addtripform--form">
+        <TextField
+          name="Departure"
+          required
+          id="dptAirport"
+          label="Departure Airport"
+          autoFocus
+        />
+        <TextField
+          name="Destination"
+          required
+          id="dstAirport"
+          label="Destination Airport"
+          autoFocus
+        />
+        <Button className="addtripform--form--button" variant="contained">
+          Next
+        </Button>
+      </div>
+    </div>
+  );
 }
