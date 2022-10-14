1424fdea20f833ab9d027499ea661ab871fcda50
Kimberly Dijkmans <kim.14420@outlook.com>
2022-10-14 18:05:53 +0100

    Created trip components

--- src/components/Trip/Trip.js
+++ src/components/Trip/Trip.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './Trip.css';
 
 export default function Trip() {
   return (
-    <div>Trip</div>
+    <div className="trip">
+      <div className="trip__goal-chart"></div>
+      <div className="trip__text">
+        <div className="trip__text__trip-info">
+          <p className="trip__text__trip-info__airport-codes">LGW - AMS</p>
+          <p className="trip__text__trip-info__date">26 Nov. 2022</p>
+        </div>
+        <p className="trip__text__status">Pending</p>
+      </div>
+    </div>
   )
 }
