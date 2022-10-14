0d54acad44e71be6fb5ad2e543af0a6fb3f2d334
Will <willcowendrums@gmail.com>
2022-10-14 12:51:33 +0100

    html done on form component

--- src/pages/AddTripForm/AddTripForm.jsx
+++ src/pages/AddTripForm/AddTripForm.jsx
@@ -1,31 +1,43 @@
 import React from "react";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
+import './AddTripForm.css'
 
 export default function AddTripForm() {
   return (
     <div className="addtripform">
       <h1 className="addtripform--title">Add Trip</h1>
       <h3 className="addtripform--subtitle">Enter Flight Details</h3>
-      <div className="addtripform--form">
-        <TextField
-          name="Departure"
-          required
-          id="dptAirport"
-          label="Departure Airport"
-          autoFocus
+      <form className="addtripform--form">
+        <input type="text" id="addtrip--form--destination" name="destination" />
+        <input type="date" id="addtrip--form--date" name="date" />
+        <select className="addtrip--form--dropdown" name="class">
+          <option value="Economy">Economy</option>
+          <option value="Business">Business</option>
+          <option value="First">First</option>
+        </select>
+        <input
+          type="checkbox"
+          id="addtrip--form--carryon--checkbox"
+          name="carryOnCheckBox"
         />
-        <TextField
-          name="Destination"
-          required
-          id="dstAirport"
-          label="Destination Airport"
-          autoFocus
+        <label for="carryOnCheckBox">Carry-on luggage</label>
+        <input
+          type="number"
+          id="addtrip--form--carryon--weight"
+          name="carryOnWeight"
         />
-        <Button className="addtripform--form--button" variant="contained">
-          Next
-        </Button>
-      </div>
+        <input
+          type="checkbox"
+          id="addtrip--form--hold--checkbox"
+          name="holdCheckBox"
+        />
+        <label for="carryOnCheckBox">Hold luggage</label>
+        <input
+          type="number"
+          id="addtrip--form--hold--weight"
+          name="holdWeight"
+        />
+      </form>
+      <button>Next</button>
     </div>
   );
 }
