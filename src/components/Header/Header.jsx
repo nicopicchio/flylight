586f101644aef9d6c17635a926e429b2a40ebaa4
Paulo Barata <pbara7a@gmail.com>
2022-10-15 20:23:38 +0100

    ECO button custom colour

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import '../Header/Header.css'
 import backIcon from '../../assets/Back-icon.svg'
 import Switch from '@mui/material/Switch';
 import { useNavigate  } from 'react-router-dom'
+import { styled } from '@mui/material/styles';
 
 export default function Header() {
   const navigate = useNavigate()
@@ -14,10 +15,33 @@ export default function Header() {
       </button>
 
       <div className='switch-container'>
-        <Switch/>
         <p className='eco-mode'>ECO</p>
+        <CustomSwitch />
       </div>
 
     </div>
   )
 }
+
+const CustomSwitch = styled((props) => (
+  <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme }) => ({
+  '& .MuiSwitch-switchBase': {
+    transitionDuration: '300ms',
+    '&.Mui-checked': {
+      transform: 'translateX(16px)',
+      color: '#00BBB0',
+      '& + .MuiSwitch-track': {
+        backgroundColor: "#00BBB0",
+        opacity: 0.3,
+      },
+    }
+  },
+  '& .MuiSwitch-track': {
+    backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
+    opacity: 1,
+    transition: theme.transitions.create(['background-color'], {
+      duration: 500,
+    }),
+  },
+}));
