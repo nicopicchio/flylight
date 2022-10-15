361e76cf2dbf590716acd0ed81c0987377e07711
Kimberly Dijkmans <kim.14420@outlook.com>
2022-10-15 08:01:26 +0100

    Imported goal chart ring

--- src/components/Trip/Trip.js
+++ src/components/Trip/Trip.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Trip.css';
+import GoalChart from '../../components/GoalChart/GoalChart';
 
 export default function Trip() {
   return (
     <div className="trip">
-      <div className="trip__goal-chart"></div>
+      <div className="trip__goal-chart">
+        <GoalChart />
+      </div>
       <div className="trip__text">
         <div className="trip__text__trip-info">
           <p className="trip__text__trip-info__airport-codes">LGW - AMS</p>
