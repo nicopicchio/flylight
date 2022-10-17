a5cf5b048390c61e169950b2d34e94abf89d717a
Paulo Barata <pbara7a@gmail.com>
2022-10-17 11:25:05 +0100

    Extracted custom switch

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import '../Header/Header.css'
 import backIcon from '../../assets/Back-icon.svg'
-import Switch from '@mui/material/Switch';
+import CustomSwitch from '../CustomSwitch';
 import { useNavigate  } from 'react-router-dom'
-import { styled } from '@mui/material/styles';
 
 export default function Header() {
   const navigate = useNavigate()
@@ -22,26 +21,3 @@ export default function Header() {
     </div>
   )
 }
-
-const CustomSwitch = styled((props) => (
-  <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
-))(({ theme }) => ({
-  '& .MuiSwitch-switchBase': {
-    transitionDuration: '300ms',
-    '&.Mui-checked': {
-      transform: 'translateX(16px)',
-      color: '#00BBB0',
-      '& + .MuiSwitch-track': {
-        backgroundColor: "#00BBB0",
-        opacity: 0.3,
-      },
-    }
-  },
-  '& .MuiSwitch-track': {
-    backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
-    opacity: 1,
-    transition: theme.transitions.create(['background-color'], {
-      duration: 500,
-    }),
-  },
-}));
