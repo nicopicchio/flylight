40911236705b10ab8e93a3b94c3433f5666e0c85
Kimberly Dijkmans <kim.14420@outlook.com>
2023-05-09 13:44:57 +0200

    added trip data

---
+++ src/data/tripData.js
@@ -0,0 +1,58 @@
+export const trips = [
+    {
+        from: 'LGW',
+        to: 'NCE',
+        date: '26 Jun. 2023',
+        class: 'economy',
+        holdLuggage: true,
+        holdLuggageWeightAllowance: 23,
+        holdLuggageGoal: 15,
+        carbonFootPrint: 110,
+        verified: false
+    },
+    {
+        from: 'BCN',
+        to: 'CDG',
+        date: '14 Aug. 2023',
+        class: 'first',
+        holdLuggage: true,
+        holdLuggageWeightAllowance: 30,
+        holdLuggageGoal: 22,
+        carbonFootPrint: 210,
+        verified: false
+    },
+    {
+        from: 'MUC',
+        to: 'FCO',
+        date: '27 May 2023',
+        class: 'business',
+        holdLuggage: true,
+        holdLuggageWeightAllowance: 21,
+        holdLuggageGoal: 12,
+        carbonFootPrint: 190,
+        verified: false
+    },
+    {
+        from: 'LGW',
+        to: 'AMS',
+        date: '11 Aug. 2022',
+        class: 'economy',
+        holdLuggage: true,
+        holdLuggageWeightAllowance: 23,
+        holdLuggageGoal: 18,
+        carbonFootPrint: 80,
+        verified: true
+    }
+    ,
+    {
+        from: 'ARN',
+        to: 'OSL',
+        date: '2 Mar. 2022',
+        class: 'economy',
+        holdLuggage: true,
+        holdLuggageWeightAllowance: 23,
+        holdLuggageGoal: 8,
+        carbonFootPrint: 95,
+        verified: true
+    }
+]
