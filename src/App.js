48e851eaedc8d9777b6b102bdc0237b04aa43044
Kimberly Dijkmans <kim.14420@outlook.com>
2023-05-23 19:03:14 +0100

    restructured, added back button and add to data

--- src/App.js
+++ src/App.js
@@ -1,45 +1,20 @@
 import "./App.css";
-import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import StartScreen from "./pages/StartScreen/StartScreen";
 import Home from "./pages/Home/Home";
-import Header from "./components/Header/Header";
-import AddTripForm from "./components/AddTripForm/AddTripForm";
-import SetGoals from "./pages/SetGoals/SetGoals";
 import MyRewards from "./pages/MyRewards/MyRewards";
 import Trips from "./pages/Trips/Trips";
 import MyTrip from "./pages/MyTrip/MyTrip";
+import AddTrip from "./pages/AddTrip/AddTrip";
 
 function App() {
-  const initialFormData = {
-    departure: "",
-    destination: "",
-    date: "",
-    class: "",
-    carryOnCheckBox: true,
-    carryOnWeight: "",
-    holdCheckBox: true,
-    holdWeight: "",
-  };
-
-  const [formData, setFormData] = useState(initialFormData);
-
   return (
     <div className='App'>
       <Routes>
         <Route path='/' element={<StartScreen />} />
         <Route path='/home' element={<Home />} />
-        <Route
-          path='/add-new-trip'
-          element={
-            <>
-              <Header />
-              <AddTripForm formData={formData} setFormData={setFormData} />
-            </>
-          }
-        />
+        <Route path='/add-trip'element={<AddTrip />} />
         <Route path='/my-trips-summary' element={<Trips />} />
-        <Route path='/set-goals' element={<SetGoals formData={formData} />} />
         <Route path='/trips/:id' element={<MyTrip />} />
         <Route path='/my-rewards' element={<MyRewards />} />
       </Routes>
