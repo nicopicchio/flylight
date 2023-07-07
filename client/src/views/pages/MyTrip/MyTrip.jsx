80182029f2c9f11712694c7a5a5c33c32b7f3fdc
Kimberly Dijkmans <kimberly.dijkmans@outlook.com>
2023-07-07 14:04:38 +0100

    add edit my trip form

--- client/src/views/pages/MyTrip/MyTrip.jsx
+++ client/src/views/pages/MyTrip/MyTrip.jsx
@@ -2,21 +2,30 @@ import React from 'react';
 import './MyTrip.css';
 import Header from '../../components/Header/Header';
 import TripFull from '../../components/Trip/TripFull';
-import {trips} from '../../../data/tripData';
 import { useParams } from "react-router-dom";
 
-export default function MyTrip() {
+import useTrips from "../../../hooks/useUserTrips";
+
+export default function MyTrip({ user }) {
 
   const routeParams = useParams();
   const tripId = Number(routeParams.id);
+  
+  const { trips, isLoading, error } = useTrips(user);
 
   return (
     <div className="my-trip">
         <Header/>
         <h2 className="my-trip__title">My trip</h2>
         <div className="my-trip__item">
-            {trips.filter(trip => trip.id === tripId).map((trip, index) => 
-              <TripFull key={index} tripObject={trip} />
+            {isLoading ? (
+              <span>Loading...</span>
+            ) : trips && trips.length > 0  ? (
+              trips.filter(trip => trip.id === tripId).map((trip, index) => 
+                <TripFull key={index} tripObject={trip} />
+              )
+            ) : (
+              <span>{JSON.stringify(error)}</span>
             )}
         </div>
       </div>
